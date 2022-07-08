5b1e4f3d903ec6d54b220943957a8b069aba631d
ThomasCerr <99366289+ThomasCerr@users.noreply.github.com>
2022-07-08 17:51:50 -0400

    Nav and Projects section complete

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+ <Section row nopadding>
+<LeftSection>
+  <SectionTitle main center>
+    Welcome To <br />
+    My Personal Portfolio
+  </SectionTitle>
+  <SectionText>
+    What am I offering to employers here
+  </SectionText>
+  <Button onClick = {() => window.location = 'https://google.com'}>Learn More</Button>
+</LeftSection>
+
+ </Section>
+);
+
+export default Hero;
